e4167e533efdc2505a594627d71c6f2c782fc5a8
SebastianEcheverryMejia <sebastian.echeverry@autonoma.edu.co>
2022-09-26 18:01:58 -0500

    added and modified routes and model files

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express()   
+const newApi = require("./src/routes");
 
 require("dotenv").config()
 const port = 3000 || process.env.PORT
 
-app.listen(port, () => {
-    console.log('Running in the port ', port)
-});
+app.listen(port, () => {console.log('Running in the port ', port)});
 
 mongoose
     .connect(process.env.CONNECTION_STRING_MONGODB)
-    .then(() => {
-        console.log('conexion exitosa con MongoDB');
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+    .then(() => {console.log('conexion exitosa con MongoDB')})
+    .catch((err) => {console.error(err);});
 
 
 app.use(express.json());
+newApi(app);
